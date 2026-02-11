80472af034bce3729ab5963dd384ed92ad26d893
Yazan Ad <yazan.adnof@ramaaz.com>
2026-02-11 12:27:49 +0100

    fix subtitle

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
                             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
                                 <span className="block xl:inline leading-tight">{hero.title}</span>
                             </h1>
-                            <p className="mt-3 text-base text-gray-500 dark:text-gray-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                            <p className="text-gray-500 dark:text-gray-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 mt-4 text-lg whitespace-pre-line">
                                 {hero.subtitle}
                             </p>
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
