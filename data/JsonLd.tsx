4913a16fc214114f76ce1234ac48789f8e39f84c
Yazan Ad <yazan.adnof@ramaaz.com>
2026-02-18 12:04:14 +0100

    fix

--- data/JsonLd.tsx
+++ data/JsonLd.tsx
@@ -31,12 +31,23 @@ export const businessData = {
 export const mapEmbedUrl = `https://maps.google.com/maps?q=${businessData.geo.lat},${businessData.geo.lng}&z=15&output=embed`;
 
 export default function JsonLd() {
-    const schema = {
+    const org = {
         '@context': 'https://schema.org',
-        '@type': 'PrintShop',
+        '@type': 'Organization',
         name: businessData.name,
         description: businessData.description,
-        telephone: businessData.telephone,
+        url: data.site.baseUrl,
+        logo: data.site.baseUrl + data.site.ogImage,
+        sameAs: [data.site.social.twitter, data.site.social.instagram].filter(Boolean),
+        contactPoint: [
+            {
+                '@type': 'ContactPoint',
+                telephone: businessData.telephone,
+                contactType: 'customer service',
+                areaServed: 'SA',
+                availableLanguage: ['Arabic', 'English'],
+            },
+        ],
         address: {
             '@type': 'PostalAddress',
             streetAddress: businessData.location,
@@ -49,6 +60,41 @@ export default function JsonLd() {
             longitude: businessData.geo.lng,
         },
     };
+
+    const website = {
+        '@context': 'https://schema.org',
+        '@type': 'WebSite',
+        url: data.site.baseUrl,
+        name: businessData.name,
+        potentialAction: {
+            '@type': 'SearchAction',
+            target: `${data.site.baseUrl}/?s={search_term_string}`,
+            'query-input': 'required name=search_term_string',
+        },
+    };
+
+    const contactPage = {
+        '@context': 'https://schema.org',
+        '@type': 'ContactPage',
+        url: `${data.site.baseUrl}/#contact`,
+        name: `اتصل بنا - ${businessData.name}`,
+        mainEntity: {
+            '@type': 'ContactPoint',
+            telephone: businessData.telephone,
+            contactType: 'customer service',
+        },
+    };
+
+    const webPage = {
+        '@context': 'https://schema.org',
+        '@type': 'WebPage',
+        url: data.site.baseUrl,
+        name: businessData.name,
+        description: businessData.description,
+        image: [data.site.baseUrl + data.site.ogImage],
+    };
+
+    const schema = [org, website, contactPage, webPage];
 
     return (
         <Script
